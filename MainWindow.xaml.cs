80185ecc40331170b4a247f5a3fb00dc98ae51d6
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2022-02-02 14:14:10 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Employee_details
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void getdata()
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=VISHAL;Initial Catalog=vishal;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Select * from employee_new", con);
+
+            DataTable dt = new DataTable("student");
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+            Datagridview.ItemsSource = dt.DefaultView;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=VISHAL;Initial Catalog=vishal;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("insert into employee_new(id ,name,date_of_Birth) values(@id,@name,@date_of_birth) ", con);
+
+            cmd.Parameters.AddWithValue("@id", textid.Text);
+            cmd.Parameters.AddWithValue("@name", textname.Text);
+            cmd.Parameters.AddWithValue("@date_of_birth", textdateofbirth.Text.ToString());
+
+
+
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            getdata();
+
+            MessageBox.Show("Data inserted");
+
+
+        }
+
+
+        private void update_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=VISHAL;Initial Catalog=vishal;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("update employee_new set name = @name ,date_of_Birth=@date_of_birth where id = @id", con);
+            cmd.Parameters.AddWithValue("@id", textid.Text);
+            cmd.Parameters.AddWithValue("@name", textname.Text);
+            cmd.Parameters.AddWithValue("@date_of_birth", textdateofbirth.Text.ToString());
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Data updated");
+            getdata();
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            getdata();
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=VISHAL;Initial Catalog=vishal;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Delete from employee_new where id = @id", con);
+            cmd.Parameters.AddWithValue("@id", textid.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            getdata();
+            MessageBox.Show("Data deleted Sucessfully");
+
+        }
+
+
+
+    }
+
+
+}
+
